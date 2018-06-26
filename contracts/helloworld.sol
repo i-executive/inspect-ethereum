244f5817acea2b1ef6daffe2eefc550d120c221e
kao <kao@ryokai.jp>
2018-06-27 02:05:53 +0900

    implement helloworld contract

---
+++ contracts/helloworld.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+contract helloworld {
+    string message;
+
+    constructor() public {
+        message = "Hello World!";
+    }
+
+    function setHelloworld(string _message) public {
+        message = _message;
+    }
+
+    function getHelloworld() public view returns (string) {
+        return message;
+    }
+}
